d5b720507b5927bad83f17e143c31d0c866f3ef5
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-04-02 18:54:43 +0530

    refactor: valid url state

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import { getQueryParams, isHttpUrl } from '../src/url';
 
 export default function Home() {
     const [url, setUrl] = useState('');
+    const isValidUrl = isHttpUrl(url);
 
     useEffect(() => {
         const appParams = getQueryParams(window.location.search);
@@ -26,16 +27,16 @@ export default function Home() {
                 <p className="prompt">{
                     url === ''
                         ? 'Enter a URL above'
-                        : (!isHttpUrl(url)
+                        : (!isValidUrl
                             ? 'This URL is not valid!'
                             : 'Your URL is broken down below')
                 }</p>
-                <input className="protocol" disabled={!isHttpUrl(url)} />
-                <input className="hostname" disabled={!isHttpUrl(url)} />
-                <input className="port" disabled={!isHttpUrl(url)} />
-                <input className="path" disabled={!isHttpUrl(url)} />
+                <input className="protocol" disabled={!isValidUrl} />
+                <input className="hostname" disabled={!isValidUrl} />
+                <input className="port" disabled={!isValidUrl} />
+                <input className="path" disabled={!isValidUrl} />
                 <ul className="query"></ul>
-                <input className="fragment" disabled={!isHttpUrl(url)} />
+                <input className="fragment" disabled={!isValidUrl} />
             </main>
         </div>
     )
