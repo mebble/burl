bb933c21fd5e936ce3b6aabea463f30ee8dde704
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-04-02 15:33:01 +0530

    more cases

--- cypress/integration/url_spec.js
+++ cypress/integration/url_spec.js
@@ -6,6 +6,11 @@ describe('getQueryParams', () => {
         expect(params).to.deep.equal(new Map())
     })
 
+    it('should return an empty map for a query string not beginning with a query string separator', () => {
+        const params = getQueryParams('a=cat&b=dog')
+        expect(params).to.deep.equal(new Map())
+    })
+
     it('should return an empty map for a URL without a query string', () => {
         const params = getQueryParams('http://example.com/some/path')
         expect(params).to.deep.equal(new Map())
@@ -29,6 +34,15 @@ describe('getQueryParams', () => {
         const params = getQueryParams('http://example.com/some/path?a=cat?&b=dog')
         const expected = new Map([
             ['a', 'cat?'],
+            ['b', 'dog']
+        ])
+        expect(params).to.deep.equal(expected)
+    })
+
+    it('should return a map of the query params of a query string beginning with a query string separator', () => {
+        const params = getQueryParams('?a=cat&b=dog')
+        const expected = new Map([
+            ['a', 'cat'],
             ['b', 'dog']
         ])
         expect(params).to.deep.equal(expected)
