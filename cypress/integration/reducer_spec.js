5a889814c8356d7761c128d07eca09c6b8380d32
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-04-09 22:18:19 +0530

    update url in reducer for some actions

--- cypress/integration/reducer_spec.js
+++ cypress/integration/reducer_spec.js
@@ -2,8 +2,8 @@ import { urlReducer } from '../../src/reducers'
 import { Url } from '../../src/types'
 
 describe('urlReducer', () => {
-    it('replaces the current urlInput', () => {
-        const oldUrl = new Url({
+    it('replaces the current url on REPLACE action', () => {
+        const current = new Url({
             protocol: 'http',
             hostname: 'url1.com',
             port: '',
@@ -21,8 +21,95 @@ describe('urlReducer', () => {
         })
         const action = { type: 'REPLACE', payload: expected }
 
-        const newUrl = urlReducer(oldUrl, action)
+        const newUrl = urlReducer(current, action)
 
         expect(newUrl).to.equal(expected)
+    })
+
+    it('replaces only the the protocol on PROTOCOL action', () => {
+        const current = new Url({
+            protocol: 'http',
+            hostname: 'url.com',
+            port: '',
+            path: '/',
+            query: new Map(),
+            fragment: '',
+        })
+        const expected = new Url({
+            protocol: 'https',
+            hostname: 'url.com',
+            port: '',
+            path: '/',
+            query: new Map(),
+            fragment: '',
+        })
+        const action = { type: 'PROTOCOL', payload: 'https' }
+
+        const newUrl = urlReducer(current, action)
+
+        expect(newUrl.protocol).to.equal(expected.protocol)
+        expect(newUrl.hostname).to.equal(expected.hostname)
+        expect(newUrl.port).to.equal(expected.port)
+        expect(newUrl.path).to.equal(expected.path)
+        expect(newUrl.query).to.deep.equal(expected.query)
+        expect(newUrl.fragment).to.equal(expected.fragment)
+    })
+
+    it('replaces only the the hostname on HOSTNAME action', () => {
+        const current = new Url({
+            protocol: 'http',
+            hostname: 'url1.com',
+            port: '',
+            path: '/',
+            query: new Map(),
+            fragment: '',
+        })
+        const expected = new Url({
+            protocol: 'http',
+            hostname: 'url2.com',
+            port: '',
+            path: '/',
+            query: new Map(),
+            fragment: '',
+        })
+        const action = { type: 'HOSTNAME', payload: 'url2.com' }
+
+        const newUrl = urlReducer(current, action)
+
+        expect(newUrl.protocol).to.equal(expected.protocol)
+        expect(newUrl.hostname).to.equal(expected.hostname)
+        expect(newUrl.port).to.equal(expected.port)
+        expect(newUrl.path).to.equal(expected.path)
+        expect(newUrl.query).to.deep.equal(expected.query)
+        expect(newUrl.fragment).to.equal(expected.fragment)
+    })
+
+    it('replaces only the the port on PORT action', () => {
+        const current = new Url({
+            protocol: 'http',
+            hostname: 'url.com',
+            port: '1000',
+            path: '/',
+            query: new Map(),
+            fragment: '',
+        })
+        const expected = new Url({
+            protocol: 'http',
+            hostname: 'url.com',
+            port: '2000',
+            path: '/',
+            query: new Map(),
+            fragment: '',
+        })
+        const action = { type: 'PORT', payload: '2000' }
+
+        const newUrl = urlReducer(current, action)
+
+        expect(newUrl.protocol).to.equal(expected.protocol)
+        expect(newUrl.hostname).to.equal(expected.hostname)
+        expect(newUrl.port).to.equal(expected.port)
+        expect(newUrl.path).to.equal(expected.path)
+        expect(newUrl.query).to.deep.equal(expected.query)
+        expect(newUrl.fragment).to.equal(expected.fragment)
     })
 })
--- src/reducers.js
+++ src/reducers.js
@@ -1,3 +1,35 @@
-export const urlReducer = (urlInput, action) => {
+import { Url } from './types'
+
+export const urlReducer = (url, action) => {
+    if (action.type === 'PROTOCOL') {
+        return new Url({
+            protocol: action.payload,
+            hostname: url.hostname,
+            port: url.port,
+            path: url.path,
+            query: url.query,
+            fragment: url.fragment,
+        });
+    }
+    if (action.type === 'HOSTNAME') {
+        return new Url({
+            protocol: url.protocol,
+            hostname: action.payload,
+            port: url.port,
+            path: url.path,
+            query: url.query,
+            fragment: url.fragment,
+        });
+    }
+    if (action.type === 'PORT') {
+        return new Url({
+            protocol: url.protocol,
+            hostname: url.hostname,
+            port: action.payload,
+            path: url.path,
+            query: url.query,
+            fragment: url.fragment,
+        });
+    }
     return action.payload;
 };
