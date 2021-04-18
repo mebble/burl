4ba85b20400bec4e9eb2db87a9ce70565b100531
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-04-18 13:12:45 +0530

    restructure query spec

--- cypress/integration/query_spec.js
+++ cypress/integration/query_spec.js
@@ -25,89 +25,68 @@ describe('adding new query param', () => {
             .should('have.value', 'dinosaur')
     })
 
-    it('should add a query param with a new key on value enter and then clear the form inputs', () => {
-        cy.get('form.query-form').within(() => {
-            cy.get(`input[name="${queryAddNames.key}"]`)
-                .type('c')
-            cy.get(`input[name="${queryAddNames.value}"]`)
-                .type('dinosaur{enter}')
-        })
-
+    context('with a new key', () => {
         const expectedQueryParams = [
             ['a', 'cat'],
             ['b', 'dog'],
             ['c', 'dinosaur'],
         ]
+        const assertQueryParamsAddedAndQueryFormCleared = () => {
+            cy.get('.query > li')
+                .should('have.length', expectedQueryParams.length)
+                .each(assertQueryParams(cy, expectedQueryParams))
 
-        cy.get('.query > li')
-            .should('have.length', expectedQueryParams.length)
-            .each(assertQueryParams(cy, expectedQueryParams))
+            cy.get('form.query-form').within(() => {
+                cy.get(`input[name="${queryAddNames.key}"]`)
+                    .should('have.value', '')
+                cy.get(`input[name="${queryAddNames.value}"]`)
+                    .should('have.value', '')
+            })
+        }
 
-        cy.get('form.query-form').within(() => {
-            cy.get(`input[name="${queryAddNames.key}"]`)
-                .should('have.value', '')
-            cy.get(`input[name="${queryAddNames.value}"]`)
-                .should('have.value', '')
+        it('should add a query param on value enter and then clear the form inputs', () => {
+            cy.get('form.query-form').within(() => {
+                cy.get(`input[name="${queryAddNames.key}"]`)
+                    .type('c')
+                cy.get(`input[name="${queryAddNames.value}"]`)
+                    .type('dinosaur{enter}')
+            })
+
+            assertQueryParamsAddedAndQueryFormCleared()
+        })
+
+        it('should add a query param on key enter and then clear the form inputs', () => {
+            cy.get('form.query-form').within(() => {
+                cy.get(`input[name="${queryAddNames.value}"]`)
+                    .type('dinosaur')
+                cy.get(`input[name="${queryAddNames.key}"]`)
+                    .type('c{enter}')
+            })
+
+            assertQueryParamsAddedAndQueryFormCleared()
+        })
+
+        it('should add a query param on button click and then clear the form inputs', () => {
+            cy.get('form.query-form').within(() => {
+                cy.get(`input[name="${queryAddNames.key}"]`)
+                    .type('c')
+                cy.get(`input[name="${queryAddNames.value}"]`)
+                    .type('dinosaur')
+                cy.get('button')
+                    .click()
+            })
+
+            assertQueryParamsAddedAndQueryFormCleared()
         })
     })
 
-    it('should add a query param with a new key on key enter and then clear the form inputs', () => {
-        cy.get('form.query-form').within(() => {
-            cy.get(`input[name="${queryAddNames.value}"]`)
-                .type('dinosaur')
-            cy.get(`input[name="${queryAddNames.key}"]`)
-                .type('c{enter}')
-        })
-
-        const expectedQueryParams = [
-            ['a', 'cat'],
-            ['b', 'dog'],
-            ['c', 'dinosaur'],
-        ]
-
-        cy.get('.query > li')
-            .should('have.length', expectedQueryParams.length)
-            .each(assertQueryParams(cy, expectedQueryParams))
-
-        cy.get('form.query-form').within(() => {
-            cy.get(`input[name="${queryAddNames.key}"]`)
-                .should('have.value', '')
-            cy.get(`input[name="${queryAddNames.value}"]`)
-                .should('have.value', '')
-        })
+    context('with an existing key', () => {
+        it('should not add a query param and not clear the form inputs')
     })
 
-    it('should add a query param with a new key on button click and then clear the form inputs', () => {
-        cy.get('form.query-form').within(() => {
-            cy.get(`input[name="${queryAddNames.key}"]`)
-                .type('c')
-            cy.get(`input[name="${queryAddNames.value}"]`)
-                .type('dinosaur')
-            cy.get('button')
-                .click()
-        })
-
-        const expectedQueryParams = [
-            ['a', 'cat'],
-            ['b', 'dog'],
-            ['c', 'dinosaur'],
-        ]
-
-        cy.get('.query > li')
-            .should('have.length', expectedQueryParams.length)
-            .each(assertQueryParams(cy, expectedQueryParams))
-
-        cy.get('form.query-form').within(() => {
-            cy.get(`input[name="${queryAddNames.key}"]`)
-                .should('have.value', '')
-            cy.get(`input[name="${queryAddNames.value}"]`)
-                .should('have.value', '')
-        })
+    context('with an empty key', () => {
+        it('should add a query param and then clear the form inputs')
     })
-
-    it('should not add a query param with an existing key and not clear the form inputs')
-
-    it('should add a query param with an empty key and then clear the form inputs')
 })
 
 describe('removing query params', () => {
