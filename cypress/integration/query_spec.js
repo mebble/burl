2ad0cd6380726713369f30c3f88ef814fa1446d7
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-04-18 13:03:34 +0530

    add query param through enter keypress

--- cypress/integration/query_spec.js
+++ cypress/integration/query_spec.js
@@ -25,7 +25,59 @@ describe('adding new query param', () => {
             .should('have.value', 'dinosaur')
     })
 
-    it('should add a query param with a new key and then clear the form inputs', () => {
+    it('should add a query param with a new key on value enter and then clear the form inputs', () => {
+        cy.get('form.query-form').within(() => {
+            cy.get(`input[name="${queryAddNames.key}"]`)
+                .type('c')
+            cy.get(`input[name="${queryAddNames.value}"]`)
+                .type('dinosaur{enter}')
+        })
+
+        const expectedQueryParams = [
+            ['a', 'cat'],
+            ['b', 'dog'],
+            ['c', 'dinosaur'],
+        ]
+
+        cy.get('.query > li')
+            .should('have.length', expectedQueryParams.length)
+            .each(assertQueryParams(cy, expectedQueryParams))
+
+        cy.get('form.query-form').within(() => {
+            cy.get(`input[name="${queryAddNames.key}"]`)
+                .should('have.value', '')
+            cy.get(`input[name="${queryAddNames.value}"]`)
+                .should('have.value', '')
+        })
+    })
+
+    it('should add a query param with a new key on key enter and then clear the form inputs', () => {
+        cy.get('form.query-form').within(() => {
+            cy.get(`input[name="${queryAddNames.value}"]`)
+                .type('dinosaur')
+            cy.get(`input[name="${queryAddNames.key}"]`)
+                .type('c{enter}')
+        })
+
+        const expectedQueryParams = [
+            ['a', 'cat'],
+            ['b', 'dog'],
+            ['c', 'dinosaur'],
+        ]
+
+        cy.get('.query > li')
+            .should('have.length', expectedQueryParams.length)
+            .each(assertQueryParams(cy, expectedQueryParams))
+
+        cy.get('form.query-form').within(() => {
+            cy.get(`input[name="${queryAddNames.key}"]`)
+                .should('have.value', '')
+            cy.get(`input[name="${queryAddNames.value}"]`)
+                .should('have.value', '')
+        })
+    })
+
+    it('should add a query param with a new key on button click and then clear the form inputs', () => {
         cy.get('form.query-form').within(() => {
             cy.get(`input[name="${queryAddNames.key}"]`)
                 .type('c')
--- src/components/QueryForm.js
+++ src/components/QueryForm.js
@@ -4,17 +4,18 @@ export default function QueryForm({ disabled, onSubmit }) {
     const [ key, setKey ] = useState('');
     const [ value, setValue ] = useState('');
 
-    const handleSubmit = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
         onSubmit({ newKey: key, newValue: value });
         setKey('');
         setValue('');
     };
 
     return (
-        <form className="query-form">
+        <form className="query-form" onSubmit={handleSubmit}>
             <input name="new-query-key" value={key} type="text" onChange={e => setKey(e.target.value)} disabled={disabled} />
             <input name="new-query-value" value={value} type="text" onChange={e => setValue(e.target.value)} disabled={disabled} />
-            <button type="button" disabled={disabled} onClick={handleSubmit}>Add</button>
+            <button type="submit" disabled={disabled}>Add</button>
         </form>
     );
 }
