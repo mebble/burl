7987b0a03a16b02aae2dcaa825a4518f1402e5ce
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-04-26 21:19:23 +0530

    use antd for QueryForm

--- src/components/QueryForm.js
+++ src/components/QueryForm.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Input, Button, Row, Col } from 'antd';
 
 export default function QueryForm({ disabled, onSubmit }) {
     const [ key, setKey ] = useState('');
@@ -13,9 +14,19 @@ export default function QueryForm({ disabled, onSubmit }) {
 
     return (
         <form className="query-form" onSubmit={handleSubmit}>
-            <input name="new-query-key" value={key} type="text" onChange={e => setKey(e.target.value)} disabled={disabled} />
-            <input name="new-query-value" value={value} type="text" onChange={e => setValue(e.target.value)} disabled={disabled} />
-            <button type="submit" disabled={disabled}>Add</button>
+            <Input.Group>
+                <Row gutter={4} justify="center">
+                    <Col span={8}>
+                        <Input name="new-query-key" value={key} type="text" onChange={e => setKey(e.target.value)} disabled={disabled} />
+                    </Col>
+                    <Col span={10}>
+                        <Input name="new-query-value" value={value} type="text" onChange={e => setValue(e.target.value)} disabled={disabled} />
+                    </Col>
+                    <Col span={4}>
+                        <Button htmlType="submit" disabled={disabled}>Add</Button>
+                    </Col>
+                </Row>
+            </Input.Group>
         </form>
     );
 }
