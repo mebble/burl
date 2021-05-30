704e486025f91b86ac812def6f3a5a76cae8bc03
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-05-30 12:38:25 +0530

    prevent query param wrapping

--- src/components/QueryParam.js
+++ src/components/QueryParam.js
@@ -1,9 +1,9 @@
-import { Input, Button, Space, Row, Col } from 'antd';
+import { Input, Button, Row, Col } from 'antd';
 
 export default function QueryParam({ name, value, disabled, onValueChange, onRemove }) {
     const ariaId = `query-${name}`;
     return (
-        <Row gutter={4} style={{ width: '100%' }}>
+        <Row gutter={4} style={{ width: '100%' }} wrap={false}>
             <Col flex="auto">
                 <Input value={value} onChange={e => onValueChange(e.target.value)} type="text" disabled={disabled} addonBefore={<span id={ariaId}>{name}</span>} aria-labelledby={ariaId} style={{ width: '100%' }}/>
             </Col>
