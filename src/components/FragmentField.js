abd921ffd37e7ced52101bd92d84194ec72a61af
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2022-10-05 21:49:50 +0530

    update styles of decoding switch

--- src/components/FragmentField.js
+++ src/components/FragmentField.js
@@ -1,17 +1,21 @@
-import { Switch } from 'antd';
+import { Switch, Row, Col  } from 'antd';
 import UrlField from './UrlField';
 
 export default function FragmentField({ name, value, disabled, onChange }) {
     return (
-        <>
-            <UrlField name={name} value={value} onChange={onChange} disabled={disabled} />
-            <Switch size="small" aria-label={`${name}-decode-url`} onClick={(checked, e) => {
-                if (checked) {
-                    onChange(decodeURIComponent(value))
-                } else {
-                    onChange(encodeURIComponent(value))
-                }
-            }} />
-        </>
+        <Row gutter={5} wrap={false}>
+            <Col flex="auto">
+                <UrlField name={name} value={value} onChange={onChange} disabled={disabled} />
+            </Col>
+            <Col style={{ display: 'flex', alignItems: 'center'}}>
+                <Switch size="small" aria-label={`${name}-decode-url`} onClick={(checked, e) => {
+                    if (checked) {
+                        onChange(decodeURIComponent(value))
+                    } else {
+                        onChange(encodeURIComponent(value))
+                    }
+                }} />
+            </Col>
+        </Row>
     );
 }
--- src/components/QueryParam.js
+++ src/components/QueryParam.js
@@ -2,15 +2,16 @@ import { Input, Button, Switch, Row, Col } from 'antd';
 
 export default function QueryParam({ name, value, disabled, onValueChange, onRemove }) {
     const ariaId = `query-${name}`;
+    const colFlex = { display: 'flex', alignItems: 'center'};
     return (
-        <Row gutter={4} style={{ width: '100%' }} wrap={false}>
+        <Row gutter={5} style={{ width: '100%' }} wrap={false}>
             <Col flex="auto">
                 <Input value={value} onChange={e => onValueChange(e.target.value)} type="text" disabled={disabled} addonBefore={<span id={ariaId}>{name}</span>} aria-labelledby={ariaId} style={{ width: '100%' }}/>
             </Col>
-            <Col>
-                <Button htmlType="button" data-query-key={name} onClick={onRemove}>-</Button>
+            <Col style={colFlex}>
+                <Button htmlType="button" size="small" shape="circle" data-query-key={name} onClick={onRemove}>-</Button>
             </Col>
-            <Col>
+            <Col style={colFlex}>
                 <Switch size="small" aria-label={`${ariaId}-decode-url`} onClick={(checked, e) => {
                     if (checked) {
                         onValueChange(decodeURIComponent(value))
